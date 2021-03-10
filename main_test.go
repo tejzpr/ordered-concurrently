f75139847b9c3ece9d7f6b32bfeb57b0cb05be2a
Tejzpr <tejzpr@gmail.com>
2021-03-10 12:27:19 -0800

    Update tests

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func workFn(val interface{}) interface{} {
 }
 
 func Test(t *testing.T) {
-	t.Run("Basic Tests", func(t *testing.T) {
+	t.Run("Test with Preset Pool Size", func(t *testing.T) {
 		max := 10
 		inputChan := make(chan *OrderedInput)
 		wg := &sync.WaitGroup{}
@@ -36,5 +36,30 @@ func Test(t *testing.T) {
 			inputChan <- input
 		}
 		wg.Wait()
+		t.Log("Test with Preset Pool Size Completed")
+	})
+	t.Run("Test with default Pool Size", func(t *testing.T) {
+		max := 10
+		inputChan := make(chan *OrderedInput)
+		wg := &sync.WaitGroup{}
+
+		outChan := Process(inputChan, workFn, &Options{})
+		go func(t *testing.T) {
+			for out := range outChan {
+				if _, ok := out.Value.(int); !ok {
+					t.Error("Invalid output")
+				}
+				wg.Done()
+			}
+		}(t)
+
+		// Create work and the associated order
+		for work := 0; work < max; work++ {
+			wg.Add(1)
+			input := &OrderedInput{work}
+			inputChan <- input
+		}
+		wg.Wait()
+		t.Log("Test with default Pool Size Completed")
 	})
 }
